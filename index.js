b03d523187d3292b3faf894546a7743a7607e783
olena.mashurova <olena.mashurova@gmail.com>
2023-07-04 17:25:02 +0300

    Lessons 1-7 passed

---
+++ index.js
@@ -0,0 +1,666 @@
+// console.log('Hello world');
+// console.log('Test');
+
+// console.log('Hello world'); console.log('Test');
+
+/*  console.log('Hello world') 
+console.log('Test')  */
+
+// {
+//   console.log("Hello world");
+//   console.log("Test");
+// }
+
+// console.log(1 + 2 + 3 + 4);
+
+/* ============Lesson 2============================== */
+
+// let name;
+// let age;
+
+// let name, age;
+
+// let name = "Kyrylo",
+//   age = 10;
+
+// let name = null,
+//   age = null;
+
+// let age = 10;
+// let name = "Kyrylo";
+
+// age = 15;
+
+// age = 20;
+
+// const price = 100;
+
+// (age = 30), (age = 40), (age = 50);
+
+// age = 30;
+// age = 40;
+
+// age = name = "Ivan";
+
+// price = 50;
+
+// var color = "#fff";
+// var color = "#000";
+
+// let color = "#fff";
+// let color = "#000";
+
+// console.log("Log:", name, age, color);
+
+/* ========Lesson 3============================ */
+
+// let n = 10;
+
+// let n = 10.55555;
+
+// let n = -10.55555;
+
+// let n = 9999999999999999999999999999999;
+
+// let n = -9999999999999999999999999999999 / "Test";
+
+// let b = NaN;
+
+// let b = NaN - 1;
+
+// let c = Infinity;
+
+// let c = -Infinity;
+
+// let m = 9999999999999999999999999999999n;
+
+// console.log(n, b, c, m);
+
+// console.log(1 / 0);
+// console.log(-1 / 0);
+
+// console.log(9999999999999999999999999999999 > Infinity);
+
+// const t = "Hello";
+// const p = "Hello";
+// const l = `Hello`;
+
+// const l = `Hello ${t} ${2 + 2} ${console.log("Let it be")}`;
+
+// const l = `Hello ${t} ${2 + 2}`;
+
+// console.log(t, p, l);
+
+// let yes = true;
+// let no = false;
+
+// console.log(yes, no);
+
+// let isAdmin = false;
+
+// let none = null;
+
+// let none;
+
+// console.log(none);
+
+// console.log(jdsfdfg);
+
+// console.log(typeof m);
+
+// console.log(isNaN(b));
+
+// console.log(isNaN(m));
+
+// console.log(isNaN(yes));
+
+// console.log(isFinite(yes));
+
+// console.log(typeof null);
+
+// console.log(typeof yes);
+
+// console.log(typeof p);
+
+/*  ======Lesson 4==================  */
+
+// const a = 1;
+// const a = 1 + 3 + 4;
+// const b = a + 2;
+
+// const b = a + 2;
+
+// const b = a + "Test";
+// const b = `${a} Test`;
+
+// console.log(b + 2);
+
+// const b = "Test" + "Hello" + `world`;
+
+// const b = "Test" + " " + "Hello" + " " + `world`;
+
+// console.log(b);
+
+// const c = 1 + "";
+// console.log(typeof c);
+
+// const c = 1;
+// console.log(c);
+
+// const a = 1 - 3 - 4;
+// const a = 1 - 3 - 4 - "Test";
+
+// const a = 1 - "10";
+// const d = -"10";
+// const d = -(-"10");
+
+// console.log(a);
+// console.log(d);
+
+// const e = -10;
+// const e = -a;
+
+// const e = a * d;
+// const e = a * "10";
+// const e = "10" * "10";
+
+// const e = 1 * "10";
+// console.log(e);
+
+// const d = "10" / 1;
+// const d = "10" / "10";
+
+// const d = 10 / "10";
+
+// console.log(d);
+
+// const f = 4 ** 5;
+// const f = "4" ** 5;
+// const f = "4text" ** 5;
+
+// const f = "4" ** "5";
+
+// const f = "4" ** 1;
+// console.log(f);
+
+// const g = 10 % 5;
+// const g = 13 % 5;
+// const g = 3 % 5;
+
+// let g = 0;
+// g = ++g;
+// g = g++;
+// console.log(g);
+
+// let f = 2;
+// let g = 0;
+// console.log(f, g);
+// f = ++g;
+// f = g++;
+
+// f = g++ + 1;
+// f = ++g + 1;
+
+// f = g-- + 1;
+// f = --g + 1;
+
+// console.log(f, g);
+
+// let f = 1;
+// console.log(f);
+
+// f *= 5;
+
+// f += 5;
+
+// f -= 5;
+
+// f *= 5;
+
+// f /= 5;
+
+// f %= 5;
+
+// console.log(f);
+
+/* ========Lesson 5=============================================== */
+
+// const personAge = 10;
+// const dogAge = 10;
+
+// console.log(personAge == dogAge);
+
+// const personAge = 10;
+// const dogAge = "10";
+
+// console.log(personAge == dogAge);
+
+// const personAge = 10;
+// const dogAge = "10g";
+
+// console.log(personAge == dogAge);
+
+// const personAge = NaN;
+// const dogAge = "10g";
+
+// console.log(personAge == dogAge);
+
+// const personAge = NaN;
+// const dogAge = NaN;
+
+// console.log(personAge == dogAge);
+
+// const personAge = "True";
+// const dogAge = "True";
+
+// console.log(personAge == dogAge);
+
+// const personAge = 10;
+// const dogAge = "True";
+
+// console.log(personAge == dogAge);
+
+// const personAge = true;
+// const dogAge = 10;
+
+// console.log(personAge == dogAge);
+
+// const personAge = true;
+// const dogAge = 1;
+
+// console.log(personAge == dogAge);
+
+// const personAge = false;
+// const dogAge = 0;
+
+// console.log(personAge == dogAge);
+
+// const personAge = 0;
+// const dogAge = "false";
+
+// console.log(personAge == dogAge);
+
+// const personAge = 0;
+// const dogAge = -0;
+
+// console.log(personAge == dogAge);
+
+// const personAge = null;
+// const dogAge = undefined;
+
+// console.log(personAge == dogAge);
+
+// const personAge = true;
+// const dogAge = undefined;
+
+// console.log(personAge == dogAge);
+
+// const personAge = null;
+// const dogAge = undefined;
+
+// console.log(personAge === dogAge);
+
+// const personAge = null;
+// const dogAge = undefined;
+
+// console.log(personAge == dogAge);
+
+// const personAge = false;
+// const dogAge = 0;
+
+// console.log(personAge === dogAge);
+
+// const personAge = false;
+// const dogAge = 0;
+
+// console.log(personAge == dogAge);
+
+// const personAge = "0";
+// const dogAge = 0;
+
+// console.log(personAge === dogAge);
+
+// const personAge = "0";
+// const dogAge = 0;
+
+// console.log(personAge != dogAge);
+
+// const personAge = "0";
+// const dogAge = 1;
+
+// console.log(personAge != dogAge);
+
+// const personAge = "0";
+// const dogAge = 0;
+
+// console.log(personAge != dogAge);
+
+// const personAge = "0";
+// const dogAge = 0;
+
+// console.log(personAge !== dogAge);
+
+// const personAge = 0;
+// const dogAge = 0;
+
+// console.log(personAge !== dogAge);
+
+// const personAge = 1;
+// const dogAge = 0;
+
+// console.log(personAge !== dogAge);
+
+// const personAge = 10;
+// const dogAge = 5;
+
+// console.log(personAge < dogAge);
+
+// const personAge = 10;
+// const dogAge = 50;
+
+// console.log(personAge < dogAge);
+
+// const personAge = 10;
+// const dogAge = "50";
+
+// console.log(personAge < dogAge);
+
+// const personAge = 10;
+// const dogAge = "50a";
+
+// console.log(personAge < dogAge);
+
+// const personAge = 10;
+// const dogAge = true;
+
+// console.log(personAge < dogAge);
+
+// const personAge = 0;
+// const dogAge = true;
+
+// console.log(personAge < dogAge);
+
+// const personAge = 0;
+// const dogAge = true;
+
+// console.log(personAge > dogAge);
+
+// const personAge = true;
+// const dogAge = 0;
+
+// console.log(personAge > dogAge);
+
+// const personAge = "true";
+// const dogAge = 0;
+
+// console.log(personAge > dogAge);
+
+// const personAge = 1;
+// const dogAge = "";
+
+// console.log(personAge > dogAge);
+
+// const personAge = false;
+// const dogAge = "Test";
+
+// console.log(personAge > dogAge);
+
+// const personAge = false;
+// const dogAge = "1";
+
+// console.log(personAge < dogAge);
+
+// const personAge = true;
+// const dogAge = "1";
+
+// console.log(personAge <= dogAge);
+
+// const personAge = true;
+// const dogAge = "0";
+
+// console.log(personAge >= dogAge);
+
+// const personAge = true;
+// const dogAge = "-1";
+
+// console.log(personAge >= dogAge);
+
+// console.log(null >= 0);
+
+// console.log(null == 0);
+
+// console.log(undefined >= 0);
+
+// console.log(1 >= 0);
+
+// console.log("1" >= "0");
+
+// console.log("hdffdgdf" >= "0");
+
+/* ==============Lesson 6=================== */
+
+// const userRole = 1;
+// const adminRole = 2;
+// const productPrice = 100;
+// const productStockPrice = 99;
+// const defaultProductPrice = 150;
+// const adminPrice = 0;
+
+// const productStockPrice = null;
+// const defaultProductPrice = null;
+// const adminPrice = null;
+
+// const productStockPrice = 0;
+// const defaultProductPrice = null;
+// const adminPrice = false;
+
+// // const productPrice =
+// //   adminPrice ?? productStockPrice ?? defaultProductPrice ?? 100;
+
+// const productPrice =
+//   adminPrice || productStockPrice || defaultProductPrice || 100;
+
+// console.log("productPrice", productPrice);
+
+// const defaultUserName = null;
+// // const ivanName = null;
+
+// // const ivanName = "Ivan name";
+
+// // const ivanName = null;
+// // const ivanName = 0;
+// // const ivanName = "";
+// // const ivanName = "false";
+// const ivanName = undefined;
+
+// const ivanRole = userRole;
+// // const ivanRole = adminRole;
+// const ivanBalance = 300;
+
+// const isAdmin = ivanRole === adminRole;
+// const canBuy = ivanBalance >= productPrice;
+
+// // const result = isAdmin === canBuy;
+
+// // const isResultTrue = result === true;
+
+// // const result1 = isAdmin === true;
+// // const result2 = canBuy === true;
+
+// // const result = result1 === result2;
+
+// const result = ivanRole === adminRole || ivanBalance >= productPrice;
+
+// console.log(result);
+
+// // const name = "" | "Ivan";
+
+// // const name = "Ivan" || null;
+
+// // const name = 0 || null;
+
+// // const name = 0 || console.log("Hello world");
+
+// // const authorName = ivanName || defaultUserName || "Автор";
+
+// // console.log(name);
+
+// // const authorName = ivanName || defaultUserName || console.log("Test");
+
+// // const authorName = ivanName || console.log("Test") || "Автор";
+
+// // const authorName = ivanRole === adminRole || "Admin";
+
+// // const authorName = ivanRole !== adminRole || "Admin";
+
+// // const authorName = ivanRole === adminRole && "Admin";
+
+// // const authorName = ivanRole === adminRole && "Admin" ?? 'User';  // Error!
+
+// // const authorName = (ivanRole === adminRole && "Admin") || "User";
+
+// // const authorName = !(ivanRole === adminRole && "Admin") || "User";
+
+// const authorName = !(ivanRole === adminRole && "Admin") && "User";
+
+// console.log("authorName", authorName);
+
+// // console.log("ivanName", !ivanName);
+// console.log("ivanName", !!ivanName);
+
+// // ===========
+
+// let productTitle = "Earphones";
+// // productTitle = "";
+
+// productTitle = null;
+
+// // productTitle = productTitle || "Назва товару";
+// // productTitle ||= "Назва товару";
+
+// // productTitle &&= "Назва товару";
+
+// productTitle ??= "Назва товару";
+
+// console.log("productTitle", productTitle);
+
+/* =======Lesson 7=========================*/
+
+// let a = 11;
+// let b = 12;
+
+// let a = 4;
+// let b = 6;
+
+// let a = 0b100;
+// let b = 0b110;
+
+// console.log(a, a.toString(2));
+// console.log(b, b.toString(2));
+
+// // let c = a & b;
+
+// let c = a | b;
+
+// console.log(c, c.toString(2));
+
+const canRead = false;
+const canWrite = true;
+const canDelete = false;
+const canEdit = true;
+
+let userRole = 0b0101;
+let adminRole = 0b1111;
+
+let user2Role = 0b0101;
+
+let user3Role = 0b0111;
+
+// let isAdmin = userRole & adminRole;
+
+let isAdmin = userRole === adminRole;
+
+// console.log(isAdmin, isAdmin.toString(2));
+
+let isSameRole = userRole & user2Role;
+
+isSameRole = userRole & user3Role;
+
+// console.log(isSameRole === user3Role);
+
+// isSameRole = (userRole & user3Role) === user3Role;
+
+// let friendRole = userRole | user3Role;
+// console.log(friendRole, friendRole.toString(2));
+
+// userRole = 0b0101;
+// user3Role = 0b0110;
+
+// let friendRole = userRole ^ user3Role;
+
+// console.log(friendRole, friendRole.toString(2));
+
+// console.log(friendRole == 0b0011);
+
+// console.log(~friendRole);
+
+// adminRole = ~adminRole;
+
+// console.log(adminRole.toString(2));
+
+// let freeTea = 0b0001;
+
+// freeTea = freeTea << 1;
+
+// console.log(freeTea.toString(2));
+
+// freeTea = freeTea << 1;
+
+// console.log(freeTea.toString(2));
+
+// freeTea = freeTea << 1;
+
+// console.log(freeTea.toString(2));
+
+// console.log(freeTea === 0b1000);
+
+// let freeTea = 0b1000;
+
+// freeTea = freeTea >> 1;
+
+// console.log(freeTea.toString(2));
+
+// freeTea = freeTea >> 1;
+
+// console.log(freeTea.toString(2));
+
+// freeTea = freeTea >> 1;
+
+// console.log(freeTea.toString(2));
+
+// console.log(freeTea === 0b0001);
+
+const a = 0b0001;
+const b = 0b0010;
+const c = 0b0100;
+
+let settings = 0b0000;
+
+settings |= a;
+// 0001
+
+settings |= b;
+// 0010
+
+console.log((settings & c) === 0b0000);
+
+// 0000
+// 0100
+// =
+// 0000
+
+console.log(!((settings & c) === 0b0000));
+
+isButtonOn = !((settings & c) === 0b0000);
+
+console.log(isButtonOn);
